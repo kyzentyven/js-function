bc83b34239a90628360220fb1ff047d1759cceb4
kyzentyven <kyzentyven@gmail.com>
2025-04-22 01:24:20 +0600

    Added the basic concepts of function parameter in JavaScript.

---
+++ parameter.js
@@ -0,0 +1,17 @@
+function eatRice(){
+    console.log('Rice is the source of carbohydrates.');
+}
+eatRice();
+
+function calculateSquare(number){
+    console.log('Value of the parameter is: ', number);
+    const square = number * number;
+    console.log('Square of the given number number is: ', square);
+}
+calculateSquare(10);
+
+function calculateAdd(num1, num2){
+    const sum = num1 + num2;
+    console.log('Sum of the given numbers are: ', sum);
+}
+calculateAdd(5, 10);
