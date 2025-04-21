72b48dbeabdcbd9f1f6acdde5b5684a8dd9e716c
kyzentyven <kyzentyven@gmail.com>
2025-04-22 00:36:47 +0600

    Added the basic concepts of function declaration in JavaScript.

---
+++ function.js
@@ -0,0 +1,13 @@
+// Function declaration
+function fanOn(){
+    console.log('Please turn the fan on.');
+}
+// Call the funtion
+fanOn();
+
+// Function declaration
+function brushTeeth(){
+    console.log('Brushing the teeth is a good habit.');
+}
+// Call the function
+brushTeeth();
