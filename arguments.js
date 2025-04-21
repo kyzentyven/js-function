260cdabcccfb19a0791e76ffcac4f08f0aef07c3
kyzentyven <kyzentyven@gmail.com>
2025-04-22 01:40:05 +0600

    Added the basic concepts of function arguments in JavaScript.

---
+++ arguments.js
@@ -0,0 +1,21 @@
+function doubleIt (number){
+    const double = number * 2;
+    console.log(number, double);
+}
+doubleIt(5);
+
+const number = 35;
+doubleIt(number);
+
+const money = 100;
+doubleIt(money);
+
+function calculateDifference (num1, num2){
+    const difference = num1 - num2;
+    console.log(num1, num2, 'Diffence between the given numbers: ', difference);
+}
+calculateDifference(10, 5);
+
+const myAge = 10;
+const motherAge = 50;
+calculateDifference(motherAge, myAge);
