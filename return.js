320a07d6790e3e72258dff70860f24b15b532c4f
kyzentyven <kyzentyven@gmail.com>
2025-04-22 13:15:13 +0600

    Added the basic concepts of function return statement in JavaScript.

---
+++ return.js
@@ -0,0 +1,17 @@
+function tenTimes (number){
+    const result = number * 10;
+    return result;
+}
+
+function cutHalf (number){
+    const half = number / 2;
+    return half;
+}
+
+tenTimes(5);
+
+const output = tenTimes(5);
+console.log('Output: ', output);
+
+const bigNumber = tenTimes(99);
+console.log('Big Number: ', bigNumber);
